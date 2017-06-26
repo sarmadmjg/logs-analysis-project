275786b6ebe2cdfba546b303819f68508aa542b6
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-26 21:21:30 +0300

    Fix formatting in tabulate.py

--- tabulate.py
+++ tabulate.py
@@ -1,36 +1,58 @@
+"""Print data in tabulated form
+"""
 
 
-def table_query(headers, rows, title):
+def table_query(headers, rows, title=''):
+    """Print query results with headers
+
+    Args:
+        headers (tuple): Headers of the table
+        rows (list): a list of tuples, represent entries in table
+        title (str): A title for the table
+    """
     vert_sep = '|'
     hor_sep = '-'
-
-    # headers = ['Real Madrid', 'Manchester United', 'Barcelona']
-    # rows = [('asdf', 'asdfeeecad', 'keidniihd'),\
-    #         ('ine,c', 'iencidsid asdf aasfds93', 'eac')]
+    col_padding = 2
 
     print()
     print(title)
     print()
 
+    # Calculate the optimal width for each column based on the headers and data
+    # These can probably fit in one line, but this is easier to debug
+
+    # Find widths of headers
     header_widths = [len(x) for x in headers]
 
-    l = len(headers)
+    ln = len(headers)
 
+    # Find max width of each column
     rows_widths = []
-    for i in range(l):
-        rows_widths.append(max([len(str(rows[x][i])) for x in range(len(rows))]))
+    for i in range(ln):
+        rows_widths.append(max([len(str(rows[x][i]))
+                                for x in range(len(rows))]))
 
-    col_widths = [max(header_widths[i], rows_widths[i]) + 2 for i in range(l)]
+    # Choose the max width for each col from the header and rows, add padding
+    col_widths = [max(header_widths[i],
+                      rows_widths[i]) + col_padding for i in range(ln)]
 
-    hor_line = vert_sep.join([hor_sep * col_widths[i] for i in range(l)])
-    headers_str = vert_sep.join(['{:^{width}}'.format(headers[i], width=col_widths[i]) for i in range(l)])
+    # Construct a horizontal ruler
+    hor_line = vert_sep.join([hor_sep * col_widths[i] for i in range(ln)])
+
+    # Construct the headers
+    headers_str = vert_sep.join(
+                    ['{:^{width}}'.format(headers[i], width=col_widths[i])
+                     for i in range(ln)])
 
     print(hor_line)
     print(headers_str)
     print(hor_line)
 
+    # Construnt and print the rows
     for row in rows:
-        row_str = vert_sep.join(['{:<{width}}'.format(str(row[i]), width=col_widths[i]) for i in range(l)])
+        row_str = vert_sep.join(
+                    ['{:<{width}}'.format(str(row[i]), width=col_widths[i])
+                     for i in range(ln)])
         print(row_str)
 
     print(hor_line)
