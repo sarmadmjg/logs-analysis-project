dfc2cf46a228b47a1c2ba716122a985d22c5d4a4
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-26 13:52:08 +0300

    Add create view query to setupdb.py

--- setupdb.py
+++ setupdb.py
@@ -7,4 +7,16 @@ def setup(cur):
     Args:
         cur (cursor): a cursor connected to the news database
     """
-    pass
+
+    # This query creates the view if it doesn't exist, or replace it if it already exist
+    # Replacing the view with each run shouldn't affect perfornamce when using PostgreSQL
+    query = """
+        create or replace view linkedlog as
+            select log.*, articles.id as article_id, articles.title as article_title,
+                authors.id as author_id, authors.name as author_name from
+            log, articles, authors
+            where log.path like ('/article/' || articles.slug || '%')
+                and articles.author = authors.id
+        """
+
+    cur.execute(query)
