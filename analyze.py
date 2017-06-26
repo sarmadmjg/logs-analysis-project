8d735328f5a2b32d94b366f2132b625c1f10d21c
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-26 22:25:22 +0300

    Display error ratios as percentages

--- analyze.py
+++ analyze.py
@@ -61,35 +61,41 @@ def top_authors(cur, lim=-1):
     return (headers, cur.fetchall())
 
 
-def days_high_error(cur, ratio = 0.01, lim = -1):
+def days_high_error(cur, ratio=0.01, lim=-1):
     """Identify the days with high error ratios
 
     Args:
         cur (cursor): a cursor connected to the news database
-        ratio (float, optional): between 0.0 and 1.0, the minimum ratio of errors/total requests
-        lim (int, optional): the max number of returned articles, negative numbers mean return all
+        ratio (float, optional): between 0.0 and 1.0
+                                 the minimum ratio of errors/total requests
+        lim (int, optional): the max number of returned articles
+                             negative numbers mean return all
 
     Returns:
-        list: a list of the days with high error ratio, sorted by percentage of errors in desc order
+        list: a list of the days with high error ratio,
+              sorted by percentage of errors in desc order
     """
 
     # A few remarks on this query:
-    # 1---  casting the first count to float is necesary bc
+    # 1---  casting the first count to float is necessary bc
     #       psql doesn't cast automatically when dividing integers
     # 2---  having is evaluated before the aggregations, so
-    #       we can't use the alias (e_ratio) in having.
+    #       we have to rewrite the functions
     query = """
         select date(time) as day,
-            count(*) filter (where status >= '400')::float / count(*) as e_ratio
-            from log
-            group by day
-            having (count(*) filter (where status >= '400')::float / count(*)) > {0}
-            order by day
-            {1}
+               round((count(*) filter (where status >= '400')::float
+                      / count(*)
+                      * 100)::numeric, 2) || '%' as e_ratio
+        from log
+        group by day
+        having (count(*) filter (where status >= '400')::float
+                    / count(*)) > {0}
+        order by day
+        {1}
         """.format(ratio, 'limit ' + str(lim) if lim >= 0 else '')
 
     cur.execute(query)
-    headers = ('Date', 'Error Ratio')
+    headers = ('Date', 'Errors')
     return (headers, cur.fetchall())
 
 
