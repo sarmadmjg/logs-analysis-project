33add4909d6994efafb550b54c74fce59a76a720
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-26 21:43:17 +0300

    Fix setupdb.py formatting

--- setupdb.py
+++ setupdb.py
@@ -1,6 +1,7 @@
 """Create Views on DB that the app might require
 """
 
+
 def setup(cur):
     """Add all views here
 
@@ -8,15 +9,19 @@ def setup(cur):
         cur (cursor): a cursor connected to the news database
     """
 
-    # This query creates the view if it doesn't exist, or replace it if it already exist
-    # Replacing the view with each run shouldn't affect perfornamce when using PostgreSQL
+    # This query creates the view if it doesn't exist
+    # Replaces it if it already exist
+    # Replacing the view shouldn't affect perfornamce in PostgreSQL
     query = """
         create or replace view linkedlog as
-            select log.*, articles.id as article_id, articles.title as article_title,
-                authors.id as author_id, authors.name as author_name from
-            log, articles, authors
-            where log.path like ('/article/' || articles.slug || '%')
-                and articles.author = authors.id
+        select  log.*,
+                articles.id as article_id,
+                articles.title as article_title,
+                authors.id as author_id,
+                authors.name as author_name
+        from log, articles, authors
+        where log.path like ('/article/' || articles.slug || '%')
+              and articles.author = authors.id
         """
 
     cur.execute(query)
