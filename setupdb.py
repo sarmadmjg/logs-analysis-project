d0c040dd8f11727086ca922c4a51d98767eb3bca
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-26 13:15:55 +0300

    Add setupdb.py

---
+++ setupdb.py
@@ -0,0 +1,10 @@
+"""Create Views on DB that the app might require
+"""
+
+def setup(cur):
+    """Add all views here
+
+    Args:
+        cur (cursor): a cursor connected to the news database
+    """
+    pass
